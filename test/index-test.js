bda8cf20250385c41d84febf1165be167c524b76
Chris Gioeli <cgioeli@gmail.com>
2018-05-03 17:31:44 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -24,8 +24,8 @@ describe('Arrays', function() {
     it('prepends a kitten to the beginning of the kittens array', function() {
       destructivelyPrependKitten("Bob")
       function destructivelyPrependKitten(kittens, element) {
-       kittens.unshift("Bob")
-       return kittens
+      kittens.unshift("Bob")
+      return kittens
      }
       expect(window.kittens).toEqual(["Bob", "Milo", "Otis", "Garfield"])
     })
