133ad85e664d8dba182e02581081f89bfd34d970
Chris Gioeli <cgioeli@gmail.com>
2018-05-03 19:06:46 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -97,7 +97,7 @@ describe('Arrays', function() {
     it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
       expect(removeFirstKitten()).toEqual(["Otis", "Garfield"])
       function removeFirstKitten() {
-       kittens = kittens.slice(1)
+       
        return kittens
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
       }
