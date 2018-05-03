09c5db53e79547358d6f91fc740f20a8039bbfaf
Chris Gioeli <cgioeli@gmail.com>
2018-05-03 19:03:05 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -85,7 +85,7 @@ describe('Arrays', function() {
       expect(removeLastKitten()).toEqual(["Milo", "Otis"])
       function removeLastKitten() {
         kittens = kittens.slice(0, kittens.length - 1)
-        kittens.slice(0)
+        kittens.slice(1)
         return kittens
       
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
