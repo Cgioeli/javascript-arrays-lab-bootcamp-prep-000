a931db1d6eaba26c45209afe74fd40c3b1026f23
Chris Gioeli <cgioeli@gmail.com>
2018-05-03 17:50:01 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -62,7 +62,7 @@ describe('Arrays', function() {
 
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
       function appendKitten() {
-        
+        kittens = 
       }
     })
   })
