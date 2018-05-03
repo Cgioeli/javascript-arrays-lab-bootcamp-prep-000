35916c4f0523362309a4fe228dbc61fc09d0c53e
Chris Gioeli <cgioeli@gmail.com>
2018-05-03 18:35:24 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -73,6 +73,7 @@ describe('Arrays', function() {
       expect(prependKitten("Arnold")).toEqual(["Arnold", "Milo", "Otis", "Garfield"])
       function prependKitten() {
         kittens = ['Arnold',... kittens]
+        
         kittens = kittens.slice(0, kittens.length - 1)
         return kittens
       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
